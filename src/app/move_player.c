6bca9ef6db207b3626a4ff90f82a303a8706ca92
kaio-matos <kaio_matos22@hotmail.com>
2022-09-28 02:43:58 +0200

    feat: add all key presses movements

--- src/app/move_player.c
+++ src/app/move_player.c
@@ -6,7 +6,7 @@
 /*   By: kmatos-s <kmatos-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 02:25:37 by kmatos-s          #+#    #+#             */
-/*   Updated: 2022/09/28 02:39:32 by kmatos-s         ###   ########.fr       */
+/*   Updated: 2022/09/28 02:43:10 by kmatos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,11 @@ void	move_player(int pressed_key)
 	player_pos = get_player_pos();
 	if (pressed_key == KEY_W || pressed_key == KEY_UP)
 		swap_char(player_pos, position(player_pos.x, player_pos.y - 1));
-	render(m());
+	if (pressed_key == KEY_S || pressed_key == KEY_DOWN)
+		swap_char(player_pos, position(player_pos.x, player_pos.y + 1));
+	if (pressed_key == KEY_A || pressed_key == KEY_LEFT)
+		swap_char(player_pos, position(player_pos.x - 1, player_pos.y));
+	if (pressed_key == KEY_D || pressed_key == KEY_RIGHT)
+		swap_char(player_pos, position(player_pos.x + 1, player_pos.y));
+	render(m()); // ! REMOVE LATER
 }
